73f7cc10905ab305edef1951aeba09b8b010b11b
MmaitsiMwale <mmaitsimwale@gmail.com>
2023-09-06 04:02:58 +0300

    readme-consolelog

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+const cIsFun = "C is fun";
+const pythonIsCool = "Python is cool";
+const jsIsAmazing = "JavaScript is amazing";
+
+console.log(cIsFun);
+console.log(pythonIsCool);
+console.log(jsIsAmazing);
