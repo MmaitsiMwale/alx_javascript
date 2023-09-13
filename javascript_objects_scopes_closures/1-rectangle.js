8b2feda88ddfd60159a7268563299b2d823e9204
MmaitsiMwale <mmaitsimwale@gmail.com>
2023-09-14 02:53:57 +0300

    rectangle with width and height

---
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,10 @@
+//A simple rectangle class with width and height
+
+class Rectangle {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+}
+
+module.exports = Rectangle;
