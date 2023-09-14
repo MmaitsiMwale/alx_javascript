de0a93c1ffdcaeb9c656297dd5b9223ff6a8016b
MmaitsiMwale <mmaitsimwale@gmail.com>
2023-09-14 03:16:28 +0300

    rectangle-on-condition

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -3,7 +3,7 @@
 class Rectangle {
     constructor(w, h) {
         if (!Number.isInteger(w) || w <= 0 || !Number.isInteger(h) || h <= 0) {
-            return {};
+            return;
         }
 
         this.width = w;
